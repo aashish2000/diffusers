c089e74b3eb72e0c08de3ca7509984f64f2bde88
Aashish Anantha Ramakrishnan <aza6352@psu.edu>
2023-10-20 02:24:01 -0400

    Add

--- examples/text_to_image/weighted_lora_train.py
+++ examples/text_to_image/weighted_lora_train.py
@@ -99,7 +99,7 @@ def create_weighted_prompt_embeds(compel, captions, weight):
             new_caption = add_prompt_weight_characters(caption_txt, key_phrases_updated, weight)
         weighted_captions.append(new_caption)
     
-    print(weighted_captions)
+    # print(weighted_captions)
 
     conditioning = compel(weighted_captions)
     return(conditioning)
