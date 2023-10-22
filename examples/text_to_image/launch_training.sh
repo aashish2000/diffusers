7325f02850c650b7ea1ef6b09c180bf2e228e054
Aashish Anantha Ramakrishnan <aza6352@psu.edu>
2023-10-21 21:01:13 -0400

    Update

--- examples/text_to_image/launch_training.sh
+++ examples/text_to_image/launch_training.sh
@@ -1,11 +1,12 @@
 export MODEL_NAME="runwayml/stable-diffusion-v1-5"
 # export TRAIN_DIR="../../../../neurips/datasets/misc_samples/test_topic/"
 # export TRAIN_DIR="../../../../neurips/datasets/non_entity_datasets/anna_ne_512/huggingface/"
-# export TRAIN_DIR="../../../../neurips/datasets/non_entity_datasets/anna_ne_caption_prefixes/objects_list/huggingface_sharpened/"
-export TRAIN_DIR="../../../../neurips/datasets/non_entity_datasets/anna_ne_sharpened_512/huggingface/"
+export TRAIN_DIR="../../../../neurips/datasets/non_entity_datasets/anna_ne_caption_prefixes/objects_list/huggingface_sharpened/"
+# export TRAIN_DIR="../../../../neurips/datasets/non_entity_datasets/anna_ne_sharpened_512/huggingface/"
 # export OUTPUT_DIR="/tmp/"
 # export OUTPUT_DIR="/tmp/"
-export OUTPUT_DIR="./models/lora_sharpened/"
+# export OUTPUT_DIR="./models/lora_sharpened/"
+export OUTPUT_DIR="./models/finetuned_lora+text_weighting+sharpened/"
 
 # accelerate launch train_text_to_image.py \
 #   --pretrained_model_name_or_path=$MODEL_NAME \
@@ -41,24 +42,7 @@ export OUTPUT_DIR="./models/lora_sharpened/"
 #   --validation_prompt="With great power comes great responsibilty." \
 #   --seed=42
 
-# accelerate launch --main_process_port=25000 weighted_lora_train.py \
-#   --mixed_precision="fp16" \
-#   --pretrained_model_name_or_path=$MODEL_NAME \
-#   --train_data_dir=$TRAIN_DIR \
-#   --dataloader_num_workers=8 \
-#   --resolution=512 --center_crop --random_flip \
-#   --train_batch_size=8 \
-#   --gradient_accumulation_steps=4 \
-#   --max_train_steps=15000 \
-#   --learning_rate=1e-04 \
-#   --max_grad_norm=1 \
-#   --lr_scheduler="cosine" --lr_warmup_steps=0 \
-#   --output_dir=${OUTPUT_DIR} \
-#   --checkpointing_steps=250 \
-#   --validation_prompt="A salad of strawberries, goat cheese and candied almonds." \
-#   --seed=42 \
-
-accelerate launch --main_process_port=25000 train_text_to_image_lora.py \
+accelerate launch --main_process_port=25000 weighted_lora_train.py \
   --mixed_precision="fp16" \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --train_data_dir=$TRAIN_DIR \
@@ -74,6 +58,23 @@ accelerate launch --main_process_port=25000 train_text_to_image_lora.py \
   --checkpointing_steps=250 \
   --validation_prompt="A salad of strawberries, goat cheese and candied almonds." \
   --seed=42 \
+
+# accelerate launch --main_process_port=25000 train_text_to_image_lora.py \
+#   --mixed_precision="fp16" \
+#   --pretrained_model_name_or_path=$MODEL_NAME \
+#   --train_data_dir=$TRAIN_DIR \
+#   --dataloader_num_workers=8 \
+#   --resolution=512 --center_crop --random_flip \
+#   --train_batch_size=8 \
+#   --gradient_accumulation_steps=4 \
+#   --max_train_steps=15000 \
+#   --learning_rate=1e-04 \
+#   --max_grad_norm=1 \
+#   --lr_scheduler="cosine" --lr_warmup_steps=0 \
+#   --output_dir=${OUTPUT_DIR} \
+#   --checkpointing_steps=250 \
+#   --validation_prompt="A salad of strawberries, goat cheese and candied almonds." \
+#   --seed=42 \
 
 
   # --enable_xformers_memory_efficient_attention
