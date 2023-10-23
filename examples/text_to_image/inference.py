c5c81bbcd04852912cb26045e62562d6150e9946
Aashish Anantha Ramakrishnan <aza6352@psu.edu>
2023-10-23 14:55:24 -0400

    Update

--- examples/text_to_image/inference.py
+++ examples/text_to_image/inference.py
@@ -57,7 +57,7 @@ def create_weighted_prompt_embeds(compel, line, weight):
 
 def generate_lora_stable_diffusion_images(checkpoint_name, flag_full_finetune, model_finetuned_path, generations_path):
     device = "cuda"
-    CAPTIONS_PATH = "../../../../datasets/non_entity_datasets/anna_ne_512/test/"
+    CAPTIONS_PATH = "../../../../neurips/datasets/non_entity_datasets/anna_ne_512/test/"
     # CAPTIONS_PATH = "../../../../neurips/datasets/non_entity_datasets/anna_ne_caption_prefixes/objects_list/test/"
     # generations_path = "./outputs/text_weighting+sharpened/test/"
     model_orig_path = "runwayml/stable-diffusion-v1-5"
