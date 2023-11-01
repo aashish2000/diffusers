4186e7fdd2f432500586c4587675f93d2413c054
Aashish Anantha Ramakrishnan <aza6352@psu.edu>
2023-10-31 20:17:38 -0400

    Add

--- examples/text_to_image/inference.py
+++ examples/text_to_image/inference.py
@@ -248,10 +248,22 @@ def generate_lora_stable_diffusion_images(checkpoint_name, flag_full_finetune, m
 #                                       generations_path="./outputs/seed_371/caption_prefix/",
 #                                       seed=371) 
 
-generate_lora_stable_diffusion_images(checkpoint_name="checkpoint-5000", 
-                                      flag_full_finetune="px", 
+# generate_lora_stable_diffusion_images(checkpoint_name="checkpoint-5000", 
+#                                       flag_full_finetune="px", 
+#                                       model_finetuned_path="../../../../neurips/methods/diffusers/examples/text_to_image/models/lora/",
+#                                       generations_path="./outputs/lora+caption_prefix/",
+#                                       seed=42) 
+
+# generate_lora_stable_diffusion_images(checkpoint_name="checkpoint-5000", 
+#                                       flag_full_finetune="px", 
+#                                       model_finetuned_path="../../../../neurips/methods/diffusers/examples/text_to_image/models/lora/",
+#                                       generations_path="./outputs/lora+caption_prefix/",
+#                                       seed=42) 
+
+generate_lora_stable_diffusion_images(checkpoint_name="checkpoint-3000", 
+                                      flag_full_finetune="", 
                                       model_finetuned_path="../../../../neurips/methods/diffusers/examples/text_to_image/models/lora/",
-                                      generations_path="./outputs/lora+caption_prefix/",
+                                      generations_path="./outputs/lora/",
                                       seed=42) 
 
 # print(clean_caption_prefix("A photo of Ohio Gov John Kasich town hall meetingGreat RoomSavage Mill complex.Ohio Gov John Kasich a GOP candidate for president held a town hall meeting April 13 in the Great Room at the historic Savage Mill complex"))
