2e63d104825d40eac33813b99c771a38a9bd81c6
Aashish Anantha Ramakrishnan <aza6352@psu.edu>
2023-11-07 00:29:14 -0500

    Add

--- examples/textual_inversion/inference.py
+++ examples/textual_inversion/inference.py
@@ -2,7 +2,7 @@ from diffusers import StableDiffusionPipeline
 import torch
 
 model_id = "runwayml/stable-diffusion-v1-5"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, use_safetensors=True, safety_checker=None).to("cuda")
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None).to("cuda")
 pipe.load_textual_inversion("./outputs/textual_inversion_brad_pitt")
 
 prompt = "A photo of <brad-pitt> man."
