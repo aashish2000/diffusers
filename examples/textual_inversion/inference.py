dbb0b8a6b8a50e99928f13e8ea7d9e1b8e620a73
Aashish Anantha Ramakrishnan <aza6352@psu.edu>
2023-11-07 00:59:19 -0500

    Add

--- examples/textual_inversion/inference.py
+++ examples/textual_inversion/inference.py
@@ -6,9 +6,10 @@ pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float
 pipe.to("cuda")
 pipe.load_textual_inversion("./outputs/textual_inversion_brad_pitt")
 
-prompt = "A photo of <brad-pitt> man."
+prompts_list = ["A photo of <brad-pitt> man.", "<brad-pitt> man leaves a Clinton Global Initiative conference discussion in September 2005"]
 
 generator = torch.Generator(device="cuda").manual_seed(42)
 
-image = pipe(prompt, generator=[generator], num_inference_steps=100).images[0]
-image.save("brad_pitt.png")
+for ind, prompt in enumerate(prompts_list):
+    image = pipe(prompt, generator=[generator], num_inference_steps=100).images[0]
+    image.save("brad_pitt" + str(ind) + ".png")
