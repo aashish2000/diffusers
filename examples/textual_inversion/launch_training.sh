ede1a298bea9ec32d3e287a2aed39f5890c37b7f
Aashish Anantha Ramakrishnan <aza6352@psu.edu>
2023-11-07 00:49:34 -0500

    Add

--- examples/textual_inversion/launch_training.sh
+++ examples/textual_inversion/launch_training.sh
@@ -9,11 +9,12 @@ accelerate launch textual_inversion.py \
   --resolution=512 \
   --train_batch_size=16 \
   --gradient_accumulation_steps=4 \
-  --max_train_steps=3000 \
+  --max_train_steps=1500 \
   --learning_rate=5.0e-04 --scale_lr \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
   --output_dir="./outputs/textual_inversion_brad_pitt" \
-  --num_vectors=5
+  --num_vectors=5 \
+  --resume_from_checkpoint="checkpoint-1500"
 
     # --push_to_hub \
