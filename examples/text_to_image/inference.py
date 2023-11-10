dba52460cf71474a31bff51d29c4ac65c1a70ccd
Aashish Anantha Ramakrishnan <aza6352@psu.edu>
2023-11-10 15:11:12 -0500

    Add

--- examples/text_to_image/inference.py
+++ examples/text_to_image/inference.py
@@ -335,9 +335,16 @@ def generate_lora_stable_diffusion_images(model_orig_path, checkpoint_name, flag
 #                                       generations_path="./outputs/seed_42/lora+text_weighting_2_1/",
 #                                       seed=42) 
 
+# generate_lora_stable_diffusion_images(model_orig_path="stabilityai/stable-diffusion-2-1",
+#                                       checkpoint_name="checkpoint-3000", 
+#                                       flag_full_finetune="tw", 
+#                                       model_finetuned_path="./models/lora_2_1/",
+#                                       generations_path="./outputs/seed_42/lora+text_weighting_2_1/",
+#                                       seed=42) 
+
 generate_lora_stable_diffusion_images(model_orig_path="stabilityai/stable-diffusion-2-1",
                                       checkpoint_name="checkpoint-3000", 
                                       flag_full_finetune="tw", 
-                                      model_finetuned_path="./models/lora_2_1/",
-                                      generations_path="./outputs/seed_42/lora+text_weighting_2_1/",
+                                      model_finetuned_path="./models/finetuned_lora+text_weighting_2_1/",
+                                      generations_path="./outputs/seed_42/finetuned_lora+text_weighting_2_1/",
                                       seed=42) 
