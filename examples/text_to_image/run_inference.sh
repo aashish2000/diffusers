6d8ab5f9b19bc80fafeb7a3420a51e384ac227e1
Aashish Anantha Ramakrishnan <aza6352@psu.edu>
2024-01-27 13:03:40 -0500

    Add

--- examples/text_to_image/run_inference.sh
+++ examples/text_to_image/run_inference.sh
@@ -1,14 +1,14 @@
 # !/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=7 \
-nohup \
-python call_inference_tw.py \
---model_finetuned_path "./models/lora/" \
---generations_path "./outputs/rebuttal/seed_42/tw1+_lora/" \
---checkpoint_name "checkpoint-3000" \
---weight "+" \
---seed 42 \
-> ./seed_42_tw1+lora.out &
+# CUDA_VISIBLE_DEVICES=7 \
+# nohup \
+# python call_inference_tw.py \
+# --model_finetuned_path "./models/lora/" \
+# --generations_path "./outputs/rebuttal/seed_42/tw1+_lora/" \
+# --checkpoint_name "checkpoint-3000" \
+# --weight "+" \
+# --seed 42 \
+# > ./seed_42_tw1+lora.out &
 
 # CUDA_VISIBLE_DEVICES=7 \
 # nohup \
@@ -50,7 +50,7 @@ python call_inference_tw.py \
 # --seed 371 \
 # > ./seed_371_tw1+lora.out &
 
-CUDA_VISIBLE_DEVICES=7 \
+CUDA_VISIBLE_DEVICES=6 \
 nohup \
 python call_inference_tw.py \
 --model_finetuned_path "./models/lora/" \
@@ -60,7 +60,7 @@ python call_inference_tw.py \
 --seed 371 \
 > ./seed_371_tw2+lora.out &
 
-CUDA_VISIBLE_DEVICES=7 \
+CUDA_VISIBLE_DEVICES=6 \
 nohup \
 python call_inference_tw.py \
 --model_finetuned_path "./models/lora/" \
