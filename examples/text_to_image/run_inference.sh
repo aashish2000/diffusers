b4e6e48078aef6c8d8d3d91b03d512f3e9b43069
Aashish Anantha Ramakrishnan <aza6352@psu.edu>
2024-01-27 13:02:07 -0500

    Add

--- examples/text_to_image/run_inference.sh
+++ examples/text_to_image/run_inference.sh
@@ -10,47 +10,47 @@ python call_inference_tw.py \
 --seed 42 \
 > ./seed_42_tw1+lora.out &
 
+# CUDA_VISIBLE_DEVICES=7 \
+# nohup \
+# python call_inference_tw.py \
+# --model_finetuned_path "./models/lora/" \
+# --generations_path "./outputs/rebuttal/seed_42/tw2+_lora/" \
+# --checkpoint_name "checkpoint-3000" \
+# --weight "++" \
+# --seed 42 \
+# > ./seed_42_tw2+lora.out &
+
+# CUDA_VISIBLE_DEVICES=1 \
+# nohup \
+# python call_inference_tw.py \
+# --model_finetuned_path "./models/lora/" \
+# --generations_path "./outputs/rebuttal/seed_42/tw3+_lora/" \
+# --checkpoint_name "checkpoint-3000" \
+# --weight "+++" \
+# --seed 42 \
+# > ./seed_42_tw3+lora.out &
+
+# CUDA_VISIBLE_DEVICES=1 \
+# nohup \
+# python call_inference_tw.py \
+# --model_finetuned_path "./models/lora/" \
+# --generations_path "./outputs/rebuttal/seed_42/tw4+_lora/" \
+# --checkpoint_name "checkpoint-3000" \
+# --weight "++++" \
+# --seed 42 \
+# > ./seed_42_tw4+lora.out &
+
+# CUDA_VISIBLE_DEVICES=0 \
+# nohup \
+# python call_inference_tw.py \
+# --model_finetuned_path "./models/lora/" \
+# --generations_path "./outputs/rebuttal/seed_371/tw1+_lora/" \
+# --checkpoint_name "checkpoint-3000" \
+# --weight "+" \
+# --seed 371 \
+# > ./seed_371_tw1+lora.out &
+
 CUDA_VISIBLE_DEVICES=7 \
-nohup \
-python call_inference_tw.py \
---model_finetuned_path "./models/lora/" \
---generations_path "./outputs/rebuttal/seed_42/tw2+_lora/" \
---checkpoint_name "checkpoint-3000" \
---weight "++" \
---seed 42 \
-> ./seed_42_tw2+lora.out &
-
-CUDA_VISIBLE_DEVICES=1 \
-nohup \
-python call_inference_tw.py \
---model_finetuned_path "./models/lora/" \
---generations_path "./outputs/rebuttal/seed_42/tw3+_lora/" \
---checkpoint_name "checkpoint-3000" \
---weight "+++" \
---seed 42 \
-> ./seed_42_tw3+lora.out &
-
-CUDA_VISIBLE_DEVICES=1 \
-nohup \
-python call_inference_tw.py \
---model_finetuned_path "./models/lora/" \
---generations_path "./outputs/rebuttal/seed_42/tw4+_lora/" \
---checkpoint_name "checkpoint-3000" \
---weight "++++" \
---seed 42 \
-> ./seed_42_tw4+lora.out &
-
-CUDA_VISIBLE_DEVICES=0 \
-nohup \
-python call_inference_tw.py \
---model_finetuned_path "./models/lora/" \
---generations_path "./outputs/rebuttal/seed_371/tw1+_lora/" \
---checkpoint_name "checkpoint-3000" \
---weight "+" \
---seed 371 \
-> ./seed_371_tw1+lora.out &
-
-CUDA_VISIBLE_DEVICES=0 \
 nohup \
 python call_inference_tw.py \
 --model_finetuned_path "./models/lora/" \
@@ -60,7 +60,7 @@ python call_inference_tw.py \
 --seed 371 \
 > ./seed_371_tw2+lora.out &
 
-CUDA_VISIBLE_DEVICES=4 \
+CUDA_VISIBLE_DEVICES=7 \
 nohup \
 python call_inference_tw.py \
 --model_finetuned_path "./models/lora/" \
@@ -70,12 +70,12 @@ python call_inference_tw.py \
 --seed 371 \
 > ./seed_371_tw3+lora.out &
 
-CUDA_VISIBLE_DEVICES=4 \
-nohup \
-python call_inference_tw.py \
---model_finetuned_path "./models/lora/" \
---generations_path "./outputs/rebuttal/seed_371/tw4+_lora/" \
---checkpoint_name "checkpoint-3000" \
---weight "++++" \
---seed 371 \
-> ./seed_371_tw4+lora.out &
+# CUDA_VISIBLE_DEVICES=4 \
+# nohup \
+# python call_inference_tw.py \
+# --model_finetuned_path "./models/lora/" \
+# --generations_path "./outputs/rebuttal/seed_371/tw4+_lora/" \
+# --checkpoint_name "checkpoint-3000" \
+# --weight "++++" \
+# --seed 371 \
+# > ./seed_371_tw4+lora.out &
