ef6848606d0d74a7ddb4302b6a4085b429972e1f
Aashish Anantha Ramakrishnan <aza6352@psu.edu>
2024-01-29 23:08:01 -0500

    Add

--- examples/text_to_image/remove_extra_gen_images.py
+++ examples/text_to_image/remove_extra_gen_images.py
@@ -14,13 +14,9 @@ GEN_PATHS = ["./outputs/rebuttal/seed_42/lora_tw/tw1+_lora/checkpoint-3000/",
 SRC_PATH = "../../../../neurips/datasets/non_entity_datasets/anna_ne_512/test/"
 source_files = [_ for _ in os.listdir(SRC_PATH) if _.endswith(".jpg")]
 
-extra_files = []
 for gen_path in GEN_PATHS:
-    extra_files = []
     for file_path in os.listdir(gen_path):
         if(file_path not in source_files):
-            extra_files.append(file_path)
-            # os.remove(gen_path + file_path)
-    print(len(extra_files))
+            os.remove(gen_path + file_path)
 
         
